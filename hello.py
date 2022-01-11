1ab292d3df2421dad4e2e398edf67cff6e47f16f
zenjiro <zenjiro0123@gmail.com>
2022-01-11 23:06:08 +0900

    天気を取得してロギング

--- hello.py
+++ hello.py
@@ -1,8 +1,12 @@
+import requests
+
 from flask import Flask
 
 app = Flask(__name__)
+app.logger.setLevel("INFO")
 
 
 @app.route("/")
 def hello():
+    app.logger.info(requests.get("https://tenki.jp/bousai/warn/3/17/1413000/").text)
     return "こんにちは！"
