f040f8c7231bcbc579d61e42c90dee675039ab14
zenjiro <zenjiro0123@gmail.com>
2022-01-15 22:31:17 +0900

    東急線と気象情報でもキーを読み込む

--- tokyulinebot.sh
+++ tokyulinebot.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 set -u
+. export-keys.inc.sh
 curl -s -S https://www.tokyu.co.jp/unten/unten2.json | jq -r .unten | sed "s/<br>/\n/g" | grep -E -v "現在\$" > .tokyulinebot-now
 [ -f .tokyulinebot-last ] && diff .tokyulinebot-now .tokyulinebot-last > /dev/null || (cat .tokyulinebot-now && ./tweet.sh post < .tokyulinebot-now)
 mv .tokyulinebot-now .tokyulinebot-last
