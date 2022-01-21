aff4f78e38e66cbab3f4ae1ae9450b1a4f1463d2
zenjiro <zenjiro0123@gmail.com>
2022-01-21 21:50:29 +0900

    1回実行されたら10秒ごとに10回動く

--- nanbulinebot.sh
+++ nanbulinebot.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 set -u -o pipefail
 . export-keys.inc.sh
-curl -s -S --location http://traininfo.jreast.co.jp/train_info/kanto.aspx | grep "<p class=\"mt10 sp_mt5 sp_mr25\">南武線" | sed -r -e "s|<[^>]+>||g" -e "s/&nbsp;//g" -e "s/[	 ^M]//g" -e "s/\r//g" > .nanbulinebot-now || echo 南武線は、現在平常運転しています。 > .nanbulinebot-now
-psql $DATABASE_URL --command="SELECT message FROM status WHERE bot = '$MY_SCREEN_NAME';" --no-align --tuples-only > .nanbulinebot-last
-diff .nanbulinebot-now .nanbulinebot-last > /dev/null || (cat .nanbulinebot-now && ./tweet.sh post < .nanbulinebot-now && psql $DATABASE_URL --command="INSERT INTO status VALUES (DEFAULT, '$MY_SCREEN_NAME', '$(cat .nanbulinebot-now)') ON CONFLICT (bot) DO UPDATE SET timestamp = DEFAULT, message = '$(cat .nanbulinebot-now)';")
+for i in {1..10}
+do
+    echo $i
+    curl -s -S --location http://traininfo.jreast.co.jp/train_info/kanto.aspx | grep "<p class=\"mt10 sp_mt5 sp_mr25\">南武線" | sed -r -e "s|<[^>]+>||g" -e "s/&nbsp;//g" -e "s/[	 ^M]//g" -e "s/\r//g" > .nanbulinebot-now || echo 南武線は、現在平常運転しています。 > .nanbulinebot-now
+    psql $DATABASE_URL --command="SELECT message FROM status WHERE bot = '$MY_SCREEN_NAME';" --no-align --tuples-only > .nanbulinebot-last
+    diff .nanbulinebot-now .nanbulinebot-last > /dev/null || (cat .nanbulinebot-now && ./tweet.sh post < .nanbulinebot-now && psql $DATABASE_URL --command="INSERT INTO status VALUES (DEFAULT, '$MY_SCREEN_NAME', '$(cat .nanbulinebot-now)') ON CONFLICT (bot) DO UPDATE SET timestamp = DEFAULT, message = '$(cat .nanbulinebot-now)';")
+    sleep 10
+done
