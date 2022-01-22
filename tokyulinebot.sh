55915ce9183533936d9d43b3ecdd0bd2e490a7d4
zenjiro <zenjiro0123@gmail.com>
2022-01-22 23:10:21 +0900

    Revert "東急線と気象情報も1分ごとに10回繰り返す。"
    This reverts commit 7010969118ae8efc0ca10c9a96a4e33fc4897ab1.

--- tokyulinebot.sh
+++ tokyulinebot.sh
@@ -1,11 +1,6 @@
 #!/bin/bash
 set -u
 . export-keys.inc.sh
-for i in {1..10}
-do
-    echo $0 $i
-    curl -s -S https://www.tokyu.co.jp/unten/unten2.json | jq -r .unten | sed "s/<br>/\n/g" | grep -E -v "現在\$" > .tokyulinebot-now
-    psql $DATABASE_URL --command="SELECT message FROM status WHERE bot = '$MY_SCREEN_NAME';" --no-align --tuples-only > .tokyulinebot-last
-    diff .tokyulinebot-now .tokyulinebot-last > /dev/null || (cat .tokyulinebot-now && ./tweet.sh post < .tokyulinebot-now && psql $DATABASE_URL --command="INSERT INTO status VALUES (DEFAULT, '$MY_SCREEN_NAME', '$(cat .tokyulinebot-now)') ON CONFLICT (bot) DO UPDATE SET timestamp = DEFAULT, message = '$(cat .tokyulinebot-now)';")
-    sleep 60
-done
+curl -s -S https://www.tokyu.co.jp/unten/unten2.json | jq -r .unten | sed "s/<br>/\n/g" | grep -E -v "現在\$" > .tokyulinebot-now
+psql $DATABASE_URL --command="SELECT message FROM status WHERE bot = '$MY_SCREEN_NAME';" --no-align --tuples-only > .tokyulinebot-last
+diff .tokyulinebot-now .tokyulinebot-last > /dev/null || (cat .tokyulinebot-now && ./tweet.sh post < .tokyulinebot-now && psql $DATABASE_URL --command="INSERT INTO status VALUES (DEFAULT, '$MY_SCREEN_NAME', '$(cat .tokyulinebot-now)') ON CONFLICT (bot) DO UPDATE SET timestamp = DEFAULT, message = '$(cat .tokyulinebot-now)';")
