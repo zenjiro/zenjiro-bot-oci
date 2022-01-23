a5ab0dbd72e6e13f40d9d9642710016eb102958c
zenjiro <zenjiro0123@gmail.com>
2022-01-23 15:14:36 +0900

    一時ファイルのファイル名に即値ではなくスクリプト名から取得したものを使う。

--- nanbulinebot.sh
+++ nanbulinebot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -u -o pipefail
 . export-keys.inc.sh
-curl -s -S --location http://traininfo.jreast.co.jp/train_info/kanto.aspx | grep "<p class=\"mt10 sp_mt5 sp_mr25\">南武線" | sed -r -e "s|<[^>]+>||g" -e "s/&nbsp;//g" -e "s/[	 ^M]//g" -e "s/\r//g" > .nanbulinebot-now || echo 南武線は、現在平常運転しています。 > .nanbulinebot-now
-psql "$DATABASE_URL" --command="SELECT message FROM status WHERE bot = '$MY_SCREEN_NAME';" --no-align --tuples-only > .nanbulinebot-last
-diff .nanbulinebot-now .nanbulinebot-last > /dev/null || (cat .nanbulinebot-now && ./tweet.sh post < .nanbulinebot-now && psql "$DATABASE_URL" --command="INSERT INTO status VALUES (DEFAULT, '$MY_SCREEN_NAME', '$(cat .nanbulinebot-now)') ON CONFLICT (bot) DO UPDATE SET timestamp = DEFAULT, message = '$(cat .nanbulinebot-now)';")
+curl -s -S --location http://traininfo.jreast.co.jp/train_info/kanto.aspx | grep "<p class=\"mt10 sp_mt5 sp_mr25\">南武線" | sed -r -e "s|<[^>]+>||g" -e "s/&nbsp;//g" -e "s/[	 ^M]//g" -e "s/\r//g" > .nanbulinebot-now || echo 南武線は、現在平常運転しています。 > .${MY_SCREEN_NAME}-now
+psql "$DATABASE_URL" --command="SELECT message FROM status WHERE bot = '$MY_SCREEN_NAME';" --no-align --tuples-only > .${MY_SCREEN_NAME}-last
+diff .${MY_SCREEN_NAME}-now .${MY_SCREEN_NAME}-last > /dev/null || (cat .${MY_SCREEN_NAME}-now && ./tweet.sh post < .${MY_SCREEN_NAME}-now && psql "$DATABASE_URL" --command="INSERT INTO status VALUES (DEFAULT, '$MY_SCREEN_NAME', '$(cat .${MY_SCREEN_NAME}-now)') ON CONFLICT (bot) DO UPDATE SET timestamp = DEFAULT, message = '$(cat .${MY_SCREEN_NAME}-now)';")
--- tokyulinebot.sh
+++ tokyulinebot.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -u
 . export-keys.inc.sh
-curl -s -S https://www.tokyu.co.jp/unten/unten2.json | jq -r .unten | sed "s/<br>/\n/g" | grep -E -v "現在\$" > .tokyulinebot-now
-psql "$DATABASE_URL" --command="SELECT message FROM status WHERE bot = '$MY_SCREEN_NAME';" --no-align --tuples-only > .tokyulinebot-last
-diff .tokyulinebot-now .tokyulinebot-last > /dev/null || (cat .tokyulinebot-now && ./tweet.sh post < .tokyulinebot-now && psql "$DATABASE_URL" --command="INSERT INTO status VALUES (DEFAULT, '$MY_SCREEN_NAME', '$(cat .tokyulinebot-now)') ON CONFLICT (bot) DO UPDATE SET timestamp = DEFAULT, message = '$(cat .tokyulinebot-now)';")
+curl -s -S https://www.tokyu.co.jp/unten/unten2.json | jq -r .unten | sed "s/<br>/\n/g" | grep -E -v "現在\$" > .${MY_SCREEN_NAME}-now
+psql "$DATABASE_URL" --command="SELECT message FROM status WHERE bot = '$MY_SCREEN_NAME';" --no-align --tuples-only > .${MY_SCREEN_NAME}-last
+diff .${MY_SCREEN_NAME}-now .${MY_SCREEN_NAME}-last > /dev/null || (cat .${MY_SCREEN_NAME}-now && ./tweet.sh post < .${MY_SCREEN_NAME}-now && psql "$DATABASE_URL" --command="INSERT INTO status VALUES (DEFAULT, '$MY_SCREEN_NAME', '$(cat .${MY_SCREEN_NAME}-now)') ON CONFLICT (bot) DO UPDATE SET timestamp = DEFAULT, message = '$(cat .${MY_SCREEN_NAME}-now)';")
