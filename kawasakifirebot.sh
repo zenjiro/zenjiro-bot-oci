d4b26b4c34d798948f0e678b6dfaa118a2b3d52b
zenjiro <zenjiro0123@gmail.com>
2022-01-24 22:48:26 +0900

    表現を修正

--- kawasakifirebot.sh
+++ kawasakifirebot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -u
-curl -s -S http://sc.city.kawasaki.jp/saigai/index.htm | nkf | grep '"point1.gif"' -A 4 | sed -E -e "s/^　? +　?|^--//" -e "s|<[^>]+>||g" -e "/^$/d" -e "s/ +$//" -e "s/です　/です。/" -e "s/ます　/ます。/" -e "s/せん　/せん。/" -e "s/さい$/さい。/"
+curl -s -S http://sc.city.kawasaki.jp/saigai/index.htm | nkf | grep '"point1.gif"' -A 4 | sed -E -e "s/^　? +　?|^--//" -e "s|<[^>]+>||g" -e "/^$/d" -e "s/ +$//" -e "s/(です|ます|せん)　/\\1。/" -e "s/(さい|した|ます)$/\\1。/" -e "s/^[月０１２３４５６７８９]+日　//" -e "s/頃　/頃に/" -e "s/　//g"
