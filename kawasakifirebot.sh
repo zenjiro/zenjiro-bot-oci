699ed96731196df7962593003ffa76c0fa7afc89
zenjiro <zenjiro0123@gmail.com>
2022-01-25 16:35:48 +0900

    「火の元の点検をしましょう」に。をつける

--- kawasakifirebot.sh
+++ kawasakifirebot.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 set -u
 . export-keys.inc.sh
-curl -s -S http://sc.city.kawasaki.jp/saigai/index.htm | nkf | grep '"point1.gif"' -A 4 | sed -E -e "s/^　? +　?|^--//" -e "s|<[^>]+>||g" -e "/^$/d" -e "s/ +$//" -e "s/(です|ます|せん)　/\\1。/g" -e "s/(さい|した|ます)$/\\1。/" -e "s/^[月０１２３４５６７８９]+日　//" -e "s/頃　/頃に/" -e "s/　//g" -e "y/０１２３４５６７８９/0123456789/" > ."${MY_SCREEN_NAME}"-now
+curl -s -S http://sc.city.kawasaki.jp/saigai/index.htm | nkf | grep '"point1.gif"' -A 4 | sed -E -e "s/^　? +　?|^--//" -e "s|<[^>]+>||g" -e "/^$/d" -e "s/ +$//" -e "s/(です|ます|せん)　/\\1。/g" -e "s/(さい|した|ます|しょう)$/\\1。/" -e "s/^[月０１２３４５６７８９]+日　//" -e "s/頃　/頃に/" -e "s/　//g" -e "y/０１２３４５６７８９/0123456789/" > ."${MY_SCREEN_NAME}"-now
 . compare-and-tweet.inc.sh
