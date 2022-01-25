8bcb866d004951081e510da89f55caefc4a2be2c
zenjiro <zenjiro0123@gmail.com>
2022-01-25 23:49:17 +0900

    東急線も結果によって色を付ける

--- tokyulinebot.sh
+++ tokyulinebot.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-curl -s -S https://www.tokyu.co.jp/unten/unten2.json | jq -r .unten | sed "s/<br>/\n/g" | grep -E -v "現在\$"
+set -u
+red=🔴
+orange=🟠
+green=🟢
+curl -s -S https://www.tokyu.co.jp/unten/unten2.json | jq -r .unten | sed "s/<br>/\n/g" | grep -E -v "現在\$" | sed -E -e "1s/(.+(運休|折返し運転))/$red\\1/" -e "1s/(^[^$red].+平常通り運転)/$green\\1/" -e "1s/(^[^$red$green])/$orange\\1/"
