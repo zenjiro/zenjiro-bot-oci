620167d9cd7a98f66237423d5fc1d8ffddf1f0ab
zenjiro <zenjiro0123@gmail.com>
2022-01-25 23:13:50 +0900

    結果をファイルに出力していたバグを修正

--- nanbulinebot.sh
+++ nanbulinebot.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 set -o pipefail
-curl -s -S --location http://traininfo.jreast.co.jp/train_info/kanto.aspx | grep "<p class=\"mt10 sp_mt5 sp_mr25\">南武線" | sed -r -e "s|<[^>]+>||g" -e "s/&nbsp;//g" -e "s/[	 ^M]//g" -e "s/\r//g" > .nanbulinebot-now || echo 南武線は、現在平常運転しています。
+curl -s -S --location http://traininfo.jreast.co.jp/train_info/kanto.aspx | grep "<p class=\"mt10 sp_mt5 sp_mr25\">南武線" | sed -r -e "s|<[^>]+>||g" -e "s/&nbsp;//g" -e "s/[	 ^M]//g" -e "s/\r//g" || echo 南武線は、現在平常運転しています。
