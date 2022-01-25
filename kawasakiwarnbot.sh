745701187631e1cec6fc55b73877f867e08a24d7
zenjiro <zenjiro0123@gmail.com>
2022-01-25 23:58:06 +0900

    気象情報も内容によって色を付ける

--- kawasakiwarnbot.sh
+++ kawasakiwarnbot.sh
@@ -1,2 +1,6 @@
 #!/bin/bash
-curl -s -S https://tenki.jp/bousai/warn/3/17/1413000/ | grep -E "<h2>.+市の警報・注意報" -A 10 | sed -E -e 's|</span><span class="is-alert">|／|g' -e "s|<[^>]+>||g" -e "s/^ +//" -e "/^$/d" -e "s/.+注意警戒事項：//" | tail -n +2
+set -u
+red=🔴
+orange=🟠
+green=🟢
+curl -s -S https://tenki.jp/bousai/warn/3/17/1413000/ | grep -E "<h2>.+市の警報・注意報" -A 10 | sed -E -e 's|</span><span class="is-alert">|／|g' -e "s|<[^>]+>||g" -e "s/^ +//" -e "/^$/d" -e "s/.+注意警戒事項：//" | tail -n +2 | sed -E -e "1s/(.+警報)/$red\\1/" -e "1s/(^[^$red].+注意報)/$orange\\1/" -e "1s/(^発表なし)/$green\\1/"
