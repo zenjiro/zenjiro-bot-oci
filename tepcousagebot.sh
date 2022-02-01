122a7f44035de0179e91572c0f59ce63359a5a1c
zenjiro <zenjiro0123@gmail.com>
2022-02-01 22:30:07 +0900

    shellcheckの警告に対応、計画停電に対応。

--- tepcousagebot.sh
+++ tepcousagebot.sh
@@ -1,7 +1,10 @@
 #!/bin/bash
 set -u
-read usage capacity saving hour < <(echo $(curl -s -S http://tepco-usage-api.appspot.com/latest.json | jq -r ".usage, .capacity, .saving, .hour"))
-percent=$(printf "%.0f\n" $(bc -l <<< "${usage}/${capacity}*100"))
-usage=$(LC_ALL=en_US.UTF-8 printf "%'d" $usage)
-capacity=$(LC_ALL=en_US.UTF-8 printf "%'d" $capacity)
-echo "${hour}時台の消費電力：${usage}万kW/${capacity}万kW（${percent}%）"
+# shellcheck disable=SC2046
+read -r usage capacity saving hour < <(curl -s -S http://tepco-usage-api.appspot.com/latest.json | jq -r ".usage, .capacity, .saving, .hour" | tr "\n" " ")
+# shellcheck disable=SC2086
+percent=$(printf "%.0f\n" "$(bc -l <<< $usage/$capacity*100)")
+usage=$(LC_ALL=en_US.UTF-8 printf "%'d" "$usage")
+capacity=$(LC_ALL=en_US.UTF-8 printf "%'d" "$capacity")
+saving=${saving/true/[計画停電中]}
+echo "${saving/false/}${hour}時台の消費電力：${usage}万kW/${capacity}万kW（${percent}%）"
