14a662c679cb50f340ab173e96356f25385b4eb1
zenjiro <zenjiro0123@gmail.com>
2022-02-03 23:37:36 +0900

    キーを読みだすときのアカウント名を小文字に変換

--- run-all.sh
+++ run-all.sh
@@ -5,10 +5,10 @@ function export_keys() {
     MY_SCREEN_NAME=$(basename "$1" .sh)
     export MY_SCREEN_NAME
     export MY_LANGUAGE=ja
-    eval export CONSUMER_KEY="\$${MY_SCREEN_NAME}_API_KEY"
-    eval export CONSUMER_SECRET="\$${MY_SCREEN_NAME}_API_SECRET_KEY"
-    eval export ACCESS_TOKEN="\$${MY_SCREEN_NAME}_ACCESS_TOKEN"
-    eval export ACCESS_TOKEN_SECRET="\$${MY_SCREEN_NAME}_ACCESS_TOKEN_SECRET"
+    eval export CONSUMER_KEY="\$${MY_SCREEN_NAME,,}_API_KEY"
+    eval export CONSUMER_SECRET="\$${MY_SCREEN_NAME,,}_API_SECRET_KEY"
+    eval export ACCESS_TOKEN="\$${MY_SCREEN_NAME,,}_ACCESS_TOKEN"
+    eval export ACCESS_TOKEN_SECRET="\$${MY_SCREEN_NAME,,}_ACCESS_TOKEN_SECRET"
 }
 
 for script in *{b,B}ot.sh
