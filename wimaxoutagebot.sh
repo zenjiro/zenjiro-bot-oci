786d44b95c59af395f6960a55b25ed1393c26169
zenjiro <zenjiro0123@gmail.com>
2022-02-03 23:49:35 +0900

    Revert "文字数を削減"
    This reverts commit 1d86b837faec8485384f201352ce2fe5ce6b0c16.

--- wimaxoutagebot.sh
+++ wimaxoutagebot.sh
@@ -3,5 +3,5 @@
 set -u
 green=🟢
 url=$(curl -s -S https://www.uqwimax.jp/information/maintenance/ | sed "/^$/d" | grep -A 40 "復旧状況" | grep "ＷｉＭＡＸ２＋通信障害" | head -1 | sed -E "s/.+<a href=\"(.+)\">.+/\\1/")
-curl -s -S $url | grep "内容" -A 15 | sed -E -e "s|</em><br />|\n|" -e "s|<[^>]+>||g" -e "/^$/d" | sed -E -e "N;s/\n/：/" -e "s/^内容：/$green/" -e "s/ＷｉＭＡＸ２＋/WiMAX2+/" -e "/対象サービス/d" -e "s/^対象エリア：//"
+curl -s -S $url | grep "内容" -A 15 | sed -E -e "s|</em><br />|\n|" -e "s|<[^>]+>||g" -e "/^$/d" | sed -E -e "N;s/\n/：/" -e "s/^内容：/$green/" -e "s/ＷｉＭＡＸ２＋/WiMAX2+/"
 echo $url
