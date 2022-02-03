0ceeb4466ad40d2f0587fe253b4eea05e8f46177
zenjiro <zenjiro0123@gmail.com>
2022-02-03 23:43:48 +0900

    障害発生の方も文字数を減らす

--- WiMAXOutageBot.sh
+++ WiMAXOutageBot.sh
@@ -3,5 +3,5 @@
 set -u
 red=🔴
 url=$(curl -s -S https://www.uqwimax.jp/information/maintenance/ | grep "ＷｉＭＡＸ２＋通信障害" | head -1 | sed -E "s/.+<a href=\"(.+)\">.+/\\1/")
-curl -s -S $url | grep "内容" -A 15 | sed -E -e "s|</em><br />|\n|" -e "s|<[^>]+>||g" -e "/^$/d" | sed -E -e "N;s/\n/：/" -e "s/^内容：/$red/" -e "s/ＷｉＭＡＸ２＋/WiMAX2+/"
+curl -s -S $url | grep "内容" -A 15 | sed -E -e "s|</em><br />|\n|" -e "s|<[^>]+>||g" -e "/^$/d" | sed -E -e "N;s/\n/：/" -e "s/^内容：/$red/" -e "s/ＷｉＭＡＸ２＋/WiMAX2+/" -e "/対象サービス/d" -e "s/^対象エリア：//"
 echo $url
