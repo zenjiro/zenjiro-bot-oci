78c9aa29be0098ad72994d21ef277e46070ef741
zenjiro <zenjiro0123@gmail.com>
2022-02-11 23:18:53 +0900

    「運転を見合わせています」「振替輸送」を赤くする

--- tokyulinebot.sh
+++ tokyulinebot.sh
@@ -3,4 +3,4 @@ set -u
 red=🔴
 orange=🟠
 green=🟢
-curl -s -S https://www.tokyu.co.jp/unten/unten2.json | jq -r .unten | sed "s/<br>/\n/g" | grep -E -v "現在\$" | sed -E -e "1s/(.+(運休|折返し運転))/$red\\1/" -e "1s/(^[^$red].+平常通り運転)/$green\\1/" -e "1s/(^[^$red$green])/$orange\\1/"
+curl -s -S https://www.tokyu.co.jp/unten/unten2.json | jq -r .unten | sed "s/<br>/\n/g" | grep -E -v "現在\$" | sed -E -e "1s/(.+(運休|折返し運転|運転を見合わせています|振替輸送))/$red\\1/" -e "1s/(^[^$red].+平常通り運転)/$green\\1/" -e "1s/(^[^$red$green])/$orange\\1/"
