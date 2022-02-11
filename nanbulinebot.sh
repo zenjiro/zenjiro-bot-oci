5ab979148cbd9af3e045fbacaa0573399ff4a6fc
zenjiro <zenjiro0123@gmail.com>
2022-02-11 23:13:42 +0900

    「運休が発生する場合があります。」を赤くしない。

--- nanbulinebot.sh
+++ nanbulinebot.sh
@@ -3,4 +3,4 @@ set -u -o pipefail
 red=🔴
 orange=🟠
 green=🟢
-curl -s -S --location http://traininfo.jreast.co.jp/train_info/kanto.aspx | grep "<p class=\"mt10 sp_mt5 sp_mr25\">南武線" | sed -r -e "s|<[^>]+>||g" -e "s/&nbsp;//g" -e "s/[	 ^M]//g" -e "s/\r//g" -e "s/(.+(運転を見合わせています|運休))/$red\\1/" -e "s/(^[^$red].+(遅れ|入場規制実施中))/$orange\\1/" -e "s/(^[^$red$orange])/$green\\1/" -e "y/０１２３４５６７８９/0123456789/" || echo "${green}南武線は、現在平常運転しています。"
+curl -s -S --location http://traininfo.jreast.co.jp/train_info/kanto.aspx | grep "<p class=\"mt10 sp_mt5 sp_mr25\">南武線" | sed -r -e "s|<[^>]+>||g" -e "s/&nbsp;//g" -e "s/[	 ^M]//g" -e "s/\r//g" -e "s/(.+(運転を見合わせています|運休となっています))/$red\\1/" -e "s/(^[^$red].+(遅れ|入場規制実施中))/$orange\\1/" -e "s/(^[^$red$orange])/$green\\1/" -e "y/０１２３４５６７８９/0123456789/" || echo "${green}南武線は、現在平常運転しています。"
