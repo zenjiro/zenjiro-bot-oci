da2033bcf3b3304eb60dae9bc250c08815b9d772
zenjiro <zenjiro0123@gmail.com>
2022-03-02 22:35:24 +0900

    ２行セットにせずにちゃんと:のあとの改行文字を削除

--- IIJmioTrblBot.sh
+++ IIJmioTrblBot.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -u
 url="https://www.iijmio.jp"$(curl -s -S https://www.iijmio.jp/info/trouble/ | grep "障害発生報告" | head -1 | sed -E -e "s/.+<a href=\"([^\"]+)\">.+/\\1/")
-curl -s -S $url | sed -E -e "s/<[^>]+>//g" -e "s/^\\s+//" -e "/^$/d" | awk "/^下記に示します/,/^影響サービス/;/^現象/,/^備考/" | grep -v -e "^影響サービス" -e "^備考" | sed -E -e "N;s/\n//" -e "s/。ご迷惑を.+/。/"
+curl -s -S $url | sed -E -e "s/<[^>]+>//g" -e "s/-->//" -e "s/^\\s+//" -e "/^$/d" -e "s/。ご迷惑を.+/。/" | awk "/^下記に示します/,/^影響サービス/;/^現象/,/^備考/" | grep -v -E -e "^＜ 記 ＞" -e "^影響サービス" -e "^備考|^以上" | tr "\n" "_" | sed "s/:_/:/g" | tr "_" "\n"
