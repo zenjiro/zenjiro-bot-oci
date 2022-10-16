3128aac927bc0bc33bbdd2f02b974924f5e1813e
zenjiro <zenjiro0123@gmail.com>
2022-10-16 14:00:39 +0000

    最新のツイート内容をデータベースではなくファイルに保存

--- run-all.sh
+++ run-all.sh
@@ -15,7 +15,6 @@ for script in *{b,B}ot.sh
 do
     export_keys "$script"
     bash "$script" | python3 truncate.py > ."${MY_SCREEN_NAME}"-now
-    psql "$DATABASE_URL" --command="SELECT message FROM status WHERE bot = '$MY_SCREEN_NAME';" --no-align --tuples-only > ."${MY_SCREEN_NAME}"-last
-    # shellcheck disable=SC2086
-    diff ."${MY_SCREEN_NAME}"-now ."${MY_SCREEN_NAME}"-last > /dev/null || (cat ."${MY_SCREEN_NAME}"-now && psql "$DATABASE_URL" --command="INSERT INTO status VALUES (DEFAULT, '${MY_SCREEN_NAME}', '$(cat .${MY_SCREEN_NAME}-now)') ON CONFLICT (bot) DO UPDATE SET timestamp = DEFAULT, message = '$(cat .${MY_SCREEN_NAME}-now)';" && ./tweet.sh post < ."${MY_SCREEN_NAME}"-now)
+    diff ."${MY_SCREEN_NAME}"-now ."${MY_SCREEN_NAME}"-last > /dev/null || (cat ."${MY_SCREEN_NAME}"-now && ./tweet.sh post < ."${MY_SCREEN_NAME}"-now)
+    mv ."${MY_SCREEN_NAME}"-{now,last}
 done
