0653738ea6ede5fdcefcd63c8857226fc0389621
zenjiro <zenjiro0123@gmail.com>
2024-04-24 21:45:45 +0900

    WiMAXOutageBotを復旧

--- WiMAXOutageBot.sh
+++ WiMAXOutageBot.sh
@@ -2,5 +2,5 @@
 # 障害発生状況
 set -u
 red=🔴
-url=$(curl -s -S https://www.uqwimax.jp/information/maintenance/ | grep "ＷｉＭＡＸ２＋通信障害" | head -1 | sed -E "s/.+<a href=\"(.+)\">.+/\\1/")
-curl -s -S $url | grep "内容" -A 15 | sed -E -e "s|</em><br />|\n|" -e "s|<[^>]+>||g" -e "/^$/d" | sed -E -e "N;s/\n/：/" -e "s/^内容：/$red/" -e "s/ＷｉＭＡＸ２＋/WiMAX2+/"
+url=https://www.uqwimax.jp/$(curl -s -S https://www.uqwimax.jp/information/maintenance/failure/area/kanto/ | sed "/^$/d" | grep -B 4 通信障害 | head -1 | sed -E "s/.+<a href=\"(.+)\" class=.+>/\\1/")
+curl -s -S $url | grep "uqv2-parts-title--md typeBdr" -A 15 | sed -E -e "s/<[^>]+>//g" -e "s/ //g" -e "/^$/d" | sed -E -e "N;s/\n/：/" -e "s/.+通信障害.+：/$red/" -e "s/ＷｉＭＡＸ２＋/WiMAX2+/"
--- wimaxoutagebot.sh
+++ wimaxoutagebot.sh
@@ -2,5 +2,5 @@
 # 復旧状況
 set -u
 green=🟢
-url=$(curl -s -S https://www.uqwimax.jp/information/maintenance/ | sed "/^$/d" | grep -A 40 "復旧状況" | grep "ＷｉＭＡＸ２＋通信障害" | head -1 | sed -E "s/.+<a href=\"(.+)\">.+/\\1/")
-curl -s -S $url | grep "内容" -A 15 | sed -E -e "s|</em><br />|\n|" -e "s|<[^>]+>||g" -e "/^$/d" | sed -E -e "N;s/\n/：/" -e "s/^内容：/$green/" -e "s/ＷｉＭＡＸ２＋/WiMAX2+/"
+url=https://www.uqwimax.jp/$(curl -s -S https://www.uqwimax.jp/information/maintenance/restore/area/kanto/ | sed "/^$/d" | grep -B 4 通信障害 | head -1 | sed -E "s/.+<a href=\"(.+)\" class=.+>/\\1/")
+curl -s -S $url | grep "uqv2-parts-title--md typeBdr" -A 15 | sed -E -e "s/<[^>]+>//g" -e "s/ //g" -e "/^$/d" | sed -E -e "N;s/\n/：/" -e "s/.+通信障害.+：/$green/" -e "s/ＷｉＭＡＸ２＋/WiMAX2+/"
