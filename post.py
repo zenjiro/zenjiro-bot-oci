23d6000b7cd03fad629caa718441a9a924a247dc
zenjiro <zenjiro0123@gmail.com>
2024-04-24 22:26:46 +0900

    クッキーの有効期限が切れたらログインし直す

--- post.py
+++ post.py
@@ -1,7 +1,20 @@
+import os
 import sys
 
 import twikit
 
+screen_name = sys.argv[1]
+message = sys.stdin.read()
 client = twikit.Client("ja-jp")
-client.load_cookies(f".{sys.argv[1].lower()}.json")
-client.create_tweet(sys.stdin.read())
+client.load_cookies(f".{screen_name.lower()}.json")
+try:
+    client.create_tweet(message)
+except twikit.errors.Unauthorized:
+    client = twikit.Client("ja-jp")
+    client.login(
+        auth_info_1=screen_name,
+        auth_info_2=os.getenv(f"{screen_name.lower()}_EMAIL"),
+        password=os.getenv(f"{screen_name.lower()}_PASSWORD"),
+    )
+    client.save_cookies(f".{screen_name.lower()}.json")
+    client.create_tweet(message)
