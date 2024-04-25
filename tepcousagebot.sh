350768b117c60dda896a370437bf3ce3a1749bc5
zenjiro <zenjiro0123@gmail.com>
2024-04-25 23:17:39 +0900

    東京電力電力供給状況APIが終わったので停止

--- tepcousagebot.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-set -u
-# shellcheck disable=SC2046
-read -r usage capacity saving hour < <(curl -s -S http://tepco-usage-api.appspot.com/latest.json | jq -r ".usage, .capacity, .saving, .hour" | tr "\n" " ")
-percent=$(((10 * 100 * usage / capacity + 5) / 10))
-usage=$(LC_ALL=en_US.UTF-8 printf "%'d" "$usage")
-capacity=$(LC_ALL=en_US.UTF-8 printf "%'d" "$capacity")
-saving=${saving/true/[計画停電中]}
-echo "${saving/false/}${hour}時台の消費電力：${usage}万kW/${capacity}万kW（${percent}%）"
