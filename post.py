ee32078315001dfe1e49f2c2667d06660df0b674
zenjiro <zenjiro0123@gmail.com>
2024-04-26 23:52:39 +0900

    自動的にパスワードでログインしない。

--- post.py
+++ post.py
@@ -6,15 +6,5 @@ import twikit
 screen_name = sys.argv[1]
 message = sys.stdin.read()
 client = twikit.Client("ja-jp")
-try:
-    client.load_cookies(f".{screen_name.lower()}.json")
-    client.create_tweet(message)
-except (twikit.errors.Unauthorized, FileNotFoundError):
-    client = twikit.Client("ja-jp")
-    client.login(
-        auth_info_1=screen_name,
-        auth_info_2=os.getenv(f"{screen_name.lower()}_EMAIL"),
-        password=os.getenv(f"{screen_name.lower()}_PASSWORD"),
-    )
-    client.save_cookies(f".{screen_name.lower()}.json")
-    client.create_tweet(message)
+client.load_cookies(f".{screen_name.lower()}.json")
+client.create_tweet(message)
