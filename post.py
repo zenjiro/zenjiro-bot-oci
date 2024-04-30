d28f4d902aaa640e287b4d18aec7276f4b0c8167
zenjiro <zenjiro0123@gmail.com>
2024-04-30 09:48:39 +0900

    twikitではなくTwitter API v2を使う。

--- post.py
+++ post.py
@@ -1,20 +1,21 @@
 import os
 import sys
 
-import twikit
+from requests_oauthlib import OAuth1Session
 
-screen_name = sys.argv[1]
-message = sys.stdin.read()
-client = twikit.Client("ja-jp")
-try:
-    client.load_cookies(f".{screen_name.lower()}.json")
-    client.create_tweet(message)
-except (twikit.errors.Unauthorized, FileNotFoundError):
-    client = twikit.Client("ja-jp")
-    client.login(
-        auth_info_1=screen_name,
-        auth_info_2=os.getenv(f"{screen_name.lower()}_EMAIL"),
-        password=os.getenv(f"{screen_name.lower()}_PASSWORD"),
+screen_name = sys.argv[1].lower()
+response = OAuth1Session(
+    os.environ.get(f"{screen_name}_API_KEY"),
+    client_secret=os.environ.get(f"{screen_name}_API_SECRET_KEY"),
+    resource_owner_key=os.environ.get(f"{screen_name}_ACCESS_TOKEN"),
+    resource_owner_secret=os.environ.get(f"{screen_name}_ACCESS_TOKEN_SECRET"),
+).post(
+    "https://api.twitter.com/2/tweets",
+    json={"text": sys.stdin.read()},
+)
+if response.status_code != 201:
+    raise Exception(
+        f"Request returned an error: {response.status_code} {response.text}"
     )
-    client.save_cookies(f".{screen_name.lower()}.json")
-    client.create_tweet(message)
+print(f"Response code: {response.status_code}")
+print(response.json())
