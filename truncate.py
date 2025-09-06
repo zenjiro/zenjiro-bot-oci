82221f155faa41ccea1111cf4990c7e2dee57cc1
zenjiro <zenjiro0123@gmail.com>
2025-09-06 23:08:29 +0900

    revert(truncate): restore original loop semantics (start at 1) and original stdin join behavior

--- truncate.py
+++ truncate.py
@@ -1,9 +1,8 @@
 import sys
 from twitter_text import parse_tweet
 
-s = sys.stdin.read().rstrip("\n")
-for i in range(0, len(s) + 1):
-    candidate = s if i == 0 else s[:-i]
-    if parse_tweet(candidate).weightedLength <= 280:
-        print(candidate)
+s = "".join(sys.stdin.readlines())
+for i in range(1, len(s)):
+    if parse_tweet(s[:-i]).weightedLength <= 280:
+        print(s[:-i])
         break
