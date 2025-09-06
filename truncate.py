ab5cb1044c0cb7620b5f4dc49559cdf0e0217083
zenjiro <zenjiro0123@gmail.com>
2025-09-06 23:09:24 +0900

    refactor(truncate): strip trailing whitespace with rstrip(); start loop at 0 to preserve content when within limit

--- truncate.py
+++ truncate.py
@@ -1,8 +1,9 @@
 import sys
 from twitter_text import parse_tweet
 
-s = "".join(sys.stdin.readlines())
-for i in range(1, len(s)):
-    if parse_tweet(s[:-i]).weightedLength <= 280:
-        print(s[:-i])
+s = "".join(sys.stdin.readlines()).rstrip()
+for i in range(0, len(s) + 1):
+    t = s if i == 0 else s[:-i]
+    if parse_tweet(t).weightedLength <= 280:
+        print(t)
         break
